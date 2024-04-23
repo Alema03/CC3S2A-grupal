0e4e2cd4247563b980ccbe00a5f22d6072aa0ba9
MarceloLZR <gerson.nunez.h@uni.pe>
2024-04-23 18:45:09 -0500

    2

---
+++ Sprint2/src/test/acceptanceTests/HopTest.java
@@ -0,0 +1,38 @@
+package test.acceptanceTests;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import production.business.AmericanCheckersGame;
+import production.business.CheckersPiece;
+import production.gui.AmericanCheckersGUI;
+
+
+public class HopTest extends TestCase {
+
+    private AmericanCheckersGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new AmericanCheckersGame();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+
+    @Test
+    public void testValidHop() {
+        game.makeMove(2,3,4,1);
+        assertTrue("Hop is Invalid", game.validMove(5,0,3,2));
+    }
+
+    @Test
+    public void testInvalidHop() {
+        game.makeMove(5,2,4,1);
+        assertFalse("Hop is Invalid", game.validMove(5,0,3,2));
+    }
+}
